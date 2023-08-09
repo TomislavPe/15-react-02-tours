5f711a9636bd436d3fc8477b8f5173617f628f21
TomislavPe <130553449+TomislavPe@users.noreply.github.com>
2023-08-09 19:55:46 +0200

    add fetch tours data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
     const [loading, setLoading] = useState(true);
     const [tours, setTours] = useState([]);
+
+    const fetchTours = async () => {
+        setLoading(true);
+        const response = await fetch(url);
+        const tours = await response.json();
+        console.log(tours);
+    };
+    
+    useEffect(()=>{
+      fetchTours();
+    },[])
+
     if (loading) {
         return (
             <main>
@@ -14,7 +26,11 @@ function App() {
             </main>
         );
     }
-    return <h2>Tours Project Setup</h2>;
+    return (
+        <main>
+            <Tours />
+        </main>
+    );
 }
 
 export default App;
